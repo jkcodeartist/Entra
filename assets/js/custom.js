3b39073443edb5ecf6165559cf7b68d196df1fc4
jkcodeartist <58467140+jkcodeartist@users.noreply.github.com>
2022-08-04 20:11:59 +0530

    iPad, Desktop changes done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -31,3 +31,33 @@ function toggleFullscreen(elem) {
 document.getElementById("btnFullscreen").addEventListener("click", function() {
   toggleFullscreen();
 });
+
+//--------------------------------------------------------------------------------
+
+$(window).on("load resize", function() {
+  if ($(window).width() <= 1199) {
+    $(".control-panel-load").replaceWith($(".control-panel"));
+  }
+});
+
+//--------------------------------------------------------------------------------
+
+$('a[href="#search"]').click(function() {
+  event.preventDefault();
+  $("#search-box").addClass("-open");
+  setTimeout(function() {
+    inputSearch.focus();
+  }, 800);
+});
+
+$('a[href="#close"]').click(function() {
+  event.preventDefault();
+  $("#search-box").removeClass("-open");
+});
+
+$(document).keyup(function(e) {
+  if (e.keyCode == 27) {
+    // escape key maps to keycode `27`
+    $("#search-box").removeClass("-open");
+  }
+});
